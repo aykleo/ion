fd542f3decc5c40dfaf31b4e1c5d708706422c0e
Leonardo Reis <leonardo.reis@medcof.tech>
2025-07-29 04:18:33 -0300

    Refactor exec package by separating command execution into distinct files
    Remove the old exec.go file and split its functionality into ion-exec.go and system-exec.go. This enhances code organization by clearly delineating between Ion command handling and system command execution, improving maintainability and readability.

--- exec/system-exec.go
+++ exec/system-exec.go
@@ -1,14 +1,11 @@
 package exec
 
 import (
-	"errors"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 
-	"github.com/aykleo/ion/config"
-	"github.com/aykleo/ion/data"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
@@ -36,7 +33,6 @@ func ExecSysCommand(command string, args []string) tea.Cmd {
 		}
 	}
 }
-
 func handleCDCommand(fullCommand string) CommandFinishedMsg {
 	parts := strings.Fields(fullCommand)
 	if len(parts) < 2 {
@@ -80,59 +76,5 @@ func handleCDCommand(fullCommand string) CommandFinishedMsg {
 		Command: fullCommand,
 		// Output:  "moved to " + currentDir,
 		NewDir: currentDir,
-	}
-}
-
-type IonCommandHandler func(args []string, configPath string, dataRef data.IData) tea.Cmd
-
-var routes = map[string]map[string]IonCommandHandler{
-	"user": {
-		"set": changeUsername,
-	},
-	"secret": {
-		"add":    addSecret,
-		"update": updateSecretValue,
-	},
-}
-
-func ExecIonCommand(args []string, dataRef data.IData) tea.Cmd {
-	configPath := config.GetConfigPath()
-
-	if args[0] == "ionize" && len(args) == 1 {
-		return func() tea.Msg {
-			return CommandFinishedMsg{
-				Command: "ionize",
-				Output:  "ionize",
-			}
-		}
-	}
-	if len(args) < 3 {
-		return func() tea.Msg {
-			return CommandFinishedMsg{
-				Err:     errors.New("ion [category] [action] <args>"),
-				Command: "ion",
-				Output:  "usage: ion [category] [action] <args>",
-				NewDir:  currentDir,
-			}
-		}
-	}
-
-	category := args[1]
-	action := args[2]
-	args = args[3:]
-
-	if categoryHandlers, categoryExists := routes[category]; categoryExists {
-		if handler, actionExists := categoryHandlers[action]; actionExists {
-			return handler(args, configPath, dataRef)
-		}
-	}
-
-	return func() tea.Msg {
-		return CommandFinishedMsg{
-			Err:     errors.New("command not found, please use ion help"),
-			Command: "ion",
-			Output:  "command not found",
-			NewDir:  currentDir,
-		}
 	}
 }
