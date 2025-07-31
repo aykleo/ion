886f0bb26e38bd934b206d336eb3f74c58db9a6f
Leonardo Reis <leonardo.reis@medcof.tech>
2025-07-31 06:51:46 -0300

    Refactor secret and user command handling by separating into dedicated files
    Remove ion-cmds.go and introduce ion-secret-cmds.go and ion-user-cmds.go for better organization. This change enhances clarity and maintainability of the command handling logic for secrets and user operations, ensuring a more modular codebase.

--- exec/ion-secret-cmds.go
+++ exec/ion-secret-cmds.go
@@ -10,29 +10,6 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-func changeUsername(args []string, configPath string, dataRef data.IData) tea.Cmd {
-	if len(args) != 1 {
-		return func() tea.Msg {
-			return CommandFinishedMsg{
-				Err:     errors.New("ion user rename <username>"),
-				Command: strings.Join(args, " "),
-				Output:  "ion user needs one argument, try [ ion user rename <username> ]",
-				NewDir:  currentDir,
-			}
-		}
-	}
-	username := args[0]
-	currentUsername := dataRef.GetUser().Username
-	dataRef.SetUsername(username, configPath)
-	return func() tea.Msg {
-		return CommandFinishedMsg{
-			Command: "ion user rename " + username,
-			Output:  "username " + currentUsername + " renamed to " + username,
-			NewDir:  currentDir,
-		}
-	}
-}
-
 func addSecret(args []string, configPath string, dataRef data.IData) tea.Cmd {
 	if len(args) < 2 {
 		return func() tea.Msg {
